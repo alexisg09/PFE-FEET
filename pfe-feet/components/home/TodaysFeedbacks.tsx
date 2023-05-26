bdd470510c524a89d7cb5ca4ed34808ba8a69098
alexisgr09 <grenieralexis56@gmail.com>
2023-05-27 01:30:30 +0200

    522df9ad-325d-4fbe-80a5-b2d999bfb215

--- pfe-feet/components/home/TodaysFeedbacks.tsx
+++ pfe-feet/components/home/TodaysFeedbacks.tsx
@@ -10,7 +10,7 @@ const TodaysFeedbacks = ({feebackCount}: TodaysFeedbacksProps) => {
     return (
         <div className="flex flex-col items-center justify-center w-80 h-40 bg-[#508C97] rounded-xl ">
         <span className="text-white text-7xl font-bold">{feebackCount}</span>
-        <h1 className="uppercase text-white pb-2 font-sans">Ajoutés aujourd'hui</h1>
+        <h1 className="uppercase text-white pb-2 font-sans">Ajoutés aujourd&apos;hui</h1>
         <Link href={'/calendar'}> {/* TODO Change route redirection to symptoms */}
             <Button bgColor="bg-slate-50" textColor="[#1F2937]" text="Ajouter"/>
         </Link>
