9ee3e11e9960f250b84d7e99355bfe74912da16f
alexisgr09 <grenieralexis56@gmail.com>
2023-05-27 05:00:04 +0200

    auto-3ee048e3-8541-4af2-9baf-a5c5d6d732da

--- pfe-feet/app/addSymptom/page.tsx
+++ pfe-feet/app/addSymptom/page.tsx
@@ -1,11 +1,8 @@
 import BackLink from "@/components/global/BackLink";
-import Title from "@/components/global/Title";
-import { useState } from "react";
 
 
 
 const addSymptom = () => {
-    const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
 return (
     <main className="flex min-h-screen min-w-screen flex-col items-center p-2">
