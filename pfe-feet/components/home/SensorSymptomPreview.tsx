c120605594cc270decfe42d11426da79dd8d86a5
LRAV-Alexis <alexis.grenier@lrav.fr>
2023-06-08 11:45:38 +0200

    Fixes

--- pfe-feet/components/home/SensorSymptomPreview.tsx
+++ pfe-feet/components/home/SensorSymptomPreview.tsx
@@ -1,23 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 type SensorSymptomPreviewProps = {
-    bgImg: string;
-    borderColor?: string;
-    href: string;
-    symptoms: any;
-}
+  bgImg: string;
+  href: string;
+  symptoms: any;
+};
 
-
-const SensorSymptomPreview = ({ bgImg, borderColor, href, symptoms }: SensorSymptomPreviewProps ) => {
-    // This should be a <Link> to current day symptom page
-        return (
-            <Link className={`w-16 h-16 rounded-md border-2 ${borderColor} bg-fixed bg-cover bg-center shrink-0`} href={{pathname: href, query: symptoms }} as={href}>
-                <Image alt="bgimg" width={64} height={64} src={bgImg} className="min-w-full min-h-full object-cover rounded-md"/>
-            </Link>
-    )
-}
+const SensorSymptomPreview = ({
+  bgImg,
+  href,
+  symptoms,
+}: SensorSymptomPreviewProps) => {
+  // This should be a <Link> to current day symptom page
+  return (
+    <Link
+      className={`w-16 h-16 rounded-md border-2 border-[#508C97] bg-fixed bg-cover bg-center shrink-0`}
+      href={{ pathname: href, query: symptoms }}
+      as={href}
+    >
+      <Image
+        alt="bgimg"
+        width={64}
+        height={64}
+        src={bgImg}
+        className="min-w-full min-h-full object-cover rounded-md"
+      />
+    </Link>
+  );
+};
 
 export default SensorSymptomPreview;
-
-
--- pfe-feet/types/types.ts
+++ pfe-feet/types/types.ts
@@ -1,108 +1,134 @@
 export type addFeelingFormType = {
-    moods: string[],
-    date: Date,
-    symptoms: Symptom[],
-    sleepDuration: number | null,
-    sleepQuality: 'good' | 'bad' | '',
-}
-
+  moods: string[];
+  date: Date;
+  symptoms: Symptom[];
+  sleepDuration: number | null;
+  sleepQuality: 'good' | 'bad' | '';
+};
 
 export interface IFeeling {
-    id: string,
-    date: Date,
-    symptoms: Symptom[],
-    isFromSensors: boolean,
+  id: string;
+  date: Date;
+  symptoms: Symptom[];
+  isFromSensors: boolean;
 }
 
 export interface IFeelingUser extends IFeeling {
-    sleepDuration: number | null,
-    sleepQuality: 'good' | 'bad' | '',
-    moods: string[],
+  sleepDuration: number | null;
+  sleepQuality: 'good' | 'bad' | '';
+  moods: string[];
 }
 
 export interface IAdvice {
-    id: string,
-    title: string,
-    description: string,
-    date: Date,
-    img: string,
-    backgroundImg: string,
-    symptomCategories: SymptomCategory[],
-    feelings?: Symptom[],
+  id: string;
+  title: string;
+  description: string;
+  date: Date;
+  img: string;
+  backgroundImg: string;
+  symptomCategories: SymptomCategory[];
+  feelings?: Symptom[];
 }
 
 export enum Symptom {
-    ALL_GOOD = 'Tout va bien',
-    BLOCKED_NOSE = 'Nez bouché',
-    BREATHING_DIFFICULTIES = 'Difficultés respiratoires',
-    BREATHLESSNESS = 'Essoufflement',
-    CONSTIPATION = 'Constipation',
-    COUGH = 'Toux',
-    CRAMPS = 'Crampes',
-    DIARRHEA = 'Diarrhée',
-    FATIGUE = 'Fatigue',
-    GASTRIC_PAIN = 'Douleurs gastriques',
-    HEADACHES = 'Maux de tête',
-    NAUSEA = 'Nausées',
-    SORE_THROAT = 'Mal de gorge',
-    SORENESS = 'Courbatures',
-    SWEATING = 'Sueurs',
-    SWELLING = 'Gonflements',
-    VISION_PROBLEMS = 'Troubles de la vision',
-    VOMITING = 'Vomissements',
+  ALL_GOOD = 'Tout va bien',
+  BLOCKED_NOSE = 'Nez bouché',
+  BREATHING_DIFFICULTIES = 'Difficultés respiratoires',
+  BREATHLESSNESS = 'Essoufflement',
+  CONSTIPATION = 'Constipation',
+  COUGH = 'Toux',
+  CRAMPS = 'Crampes',
+  DIARRHEA = 'Diarrhée',
+  FATIGUE = 'Fatigue',
+  GASTRIC_PAIN = 'Douleurs gastriques',
+  HEADACHES = 'Maux de tête',
+  NAUSEA = 'Nausées',
+  SORE_THROAT = 'Mal de gorge',
+  SORENESS = 'Courbatures',
+  SWEATING = 'Sueurs',
+  SWELLING = 'Gonflements',
+  VISION_PROBLEMS = 'Troubles de la vision',
+  VOMITING = 'Vomissements',
 }
 
 export enum SymptomCategory {
-    CIRCULATORY = 'Circulatoire',
-    DIGESTION = 'Digestion',
-    HYDRATION = 'Hydratation',
-    MISCELLANEOUS = 'Divers',
-    MUSCULAR = 'Musculaire',
+  CIRCULATORY = 'Circulatoire',
+  DIGESTION = 'Digestion',
+  HYDRATION = 'Hydratation',
+  MISCELLANEOUS = 'Divers',
+  MUSCULAR = 'Musculaire',
 }
 
 export type SymptomCategoryMapping = {
-    [key in Symptom]: SymptomCategory[];
+  [key in Symptom]: SymptomCategory[];
 };
 
 export const symptomCategoryMapping: SymptomCategoryMapping = {
-    [Symptom.ALL_GOOD]: [SymptomCategory.MISCELLANEOUS],
-    [Symptom.BLOCKED_NOSE]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
-    [Symptom.BREATHING_DIFFICULTIES]: [SymptomCategory.CIRCULATORY],
-    [Symptom.BREATHLESSNESS]: [SymptomCategory.CIRCULATORY],
-    [Symptom.CONSTIPATION]: [SymptomCategory.DIGESTION],
-    [Symptom.COUGH]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
-    [Symptom.CRAMPS]: [SymptomCategory.MUSCULAR, SymptomCategory.CIRCULATORY],
-    [Symptom.DIARRHEA]: [SymptomCategory.DIGESTION, SymptomCategory.HYDRATION],
-    [Symptom.FATIGUE]: [SymptomCategory.MUSCULAR, SymptomCategory.CIRCULATORY],
-    [Symptom.GASTRIC_PAIN]: [SymptomCategory.MUSCULAR, SymptomCategory.HYDRATION],
-    [Symptom.HEADACHES]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY, SymptomCategory.HYDRATION],
-    [Symptom.NAUSEA]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY, SymptomCategory.HYDRATION],
-    [Symptom.SORENESS]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
-    [Symptom.SORE_THROAT]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
-    [Symptom.SWEATING]: [SymptomCategory.CIRCULATORY, SymptomCategory.HYDRATION, SymptomCategory.DIGESTION],
-    [Symptom.SWELLING]: [SymptomCategory.DIGESTION, SymptomCategory.CIRCULATORY],
-    [Symptom.VISION_PROBLEMS]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
-    [Symptom.VOMITING]: [SymptomCategory.DIGESTION, SymptomCategory.HYDRATION],
+  [Symptom.ALL_GOOD]: [SymptomCategory.MISCELLANEOUS],
+  [Symptom.BLOCKED_NOSE]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+  ],
+  [Symptom.BREATHING_DIFFICULTIES]: [SymptomCategory.CIRCULATORY],
+  [Symptom.BREATHLESSNESS]: [SymptomCategory.CIRCULATORY],
+  [Symptom.CONSTIPATION]: [SymptomCategory.DIGESTION],
+  [Symptom.COUGH]: [SymptomCategory.MISCELLANEOUS, SymptomCategory.CIRCULATORY],
+  [Symptom.CRAMPS]: [SymptomCategory.MUSCULAR, SymptomCategory.CIRCULATORY],
+  [Symptom.DIARRHEA]: [SymptomCategory.DIGESTION, SymptomCategory.HYDRATION],
+  [Symptom.FATIGUE]: [SymptomCategory.MUSCULAR, SymptomCategory.CIRCULATORY],
+  [Symptom.GASTRIC_PAIN]: [SymptomCategory.MUSCULAR, SymptomCategory.HYDRATION],
+  [Symptom.HEADACHES]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+    SymptomCategory.HYDRATION,
+  ],
+  [Symptom.NAUSEA]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+    SymptomCategory.HYDRATION,
+  ],
+  [Symptom.SORENESS]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+  ],
+  [Symptom.SORE_THROAT]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+  ],
+  [Symptom.SWEATING]: [
+    SymptomCategory.CIRCULATORY,
+    SymptomCategory.HYDRATION,
+    SymptomCategory.DIGESTION,
+  ],
+  [Symptom.SWELLING]: [SymptomCategory.DIGESTION, SymptomCategory.CIRCULATORY],
+  [Symptom.VISION_PROBLEMS]: [
+    SymptomCategory.MISCELLANEOUS,
+    SymptomCategory.CIRCULATORY,
+  ],
+  [Symptom.VOMITING]: [SymptomCategory.DIGESTION, SymptomCategory.HYDRATION],
 };
 
-export const getSymptomHexCode = (symptom: Symptom): string | undefined =>  {
-    const categories = symptomCategoryMapping[symptom];
-    if (categories && categories.length > 0) {
-      const firstCategory = categories[0];
-      switch (firstCategory) {
-        case SymptomCategory.MUSCULAR:
-          return 'border-[#E1529F]';
-        case SymptomCategory.DIGESTION:
-          return 'border-[#ABE152]';
-        case SymptomCategory.CIRCULATORY:
-          return 'border-[#E15252]';
-        case SymptomCategory.HYDRATION:
-          return 'border-[#528BE1]';
-        case SymptomCategory.MISCELLANEOUS:
-          return 'border-[#508C97]';
-        default:
-          return undefined;
-      }
+export const getSymptomHexCode = (symptom: Symptom): string | undefined => {
+  const categories = symptomCategoryMapping[symptom];
+  if (categories && categories.length > 0) {
+    const firstCategory = categories[0];
+
+    console.log('categoriees');
+    console.log(categories);
+    switch (firstCategory) {
+      case SymptomCategory.MUSCULAR:
+        return 'border-[#E1529F] border-[1px]';
+      case SymptomCategory.DIGESTION:
+        return 'border-[#ABE152] border-[1px]';
+      case SymptomCategory.CIRCULATORY:
+        return 'border-[#E15252] border-[1px]';
+      case SymptomCategory.HYDRATION:
+        return 'border-[#528BE1] border-[1px]';
+      case SymptomCategory.MISCELLANEOUS:
+        return 'border-[#508C97] border-[1px]';
+      default:
+        return undefined;
     }
-    return undefined;
   }
+  return undefined;
+};
